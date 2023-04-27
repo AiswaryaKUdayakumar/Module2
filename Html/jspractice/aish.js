941842d188bdea07b5bfa10f4658c5847997ede7
AiswaryaKUdayakumar <aiswaryakuday@gmail.com>
2023-04-27 16:00:41 +0530

    april 27

---
+++ Html/jspractice/aish.js
@@ -0,0 +1,19 @@
+// document.write("Hi Hello");
+// alert("notification");
+// console.log("Welcome");
+
+// var name="Aiswarya";
+// let name1="Adithya";
+// document.write(name1);
+// document.write("<br>");
+// document.write(typeof name1);
+
+// var num=10;
+// var num1=20;
+
+var a=10;
+// a--;
+// var b=3;
+document.write(a**2);
+
+
